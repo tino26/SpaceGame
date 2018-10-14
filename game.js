6cdb3955c9c855a95714e79b2213dcab5a7a74c7
tinonino <aresfrend13@gmail.com>
2018-10-14 17:13:01 +0300

    poop

--- game.js
+++ game.js
@@ -1,14 +1,60 @@
 ﻿// Creating variables
-var myX = 0, myY = 0;
+var poop=new Image();
+poop.src="poop.png";
+
+class Player{
+	constructor(x, y, sx, sy){
+		this.x=x;
+		this.y=y;
+		this.sx=sx;
+		this.sy=sy;
+		this.velocity=0;
+		this.maxVelocity=3;
+	}
+
+	update(){
+    	if(isKeyPressed[key_right]){
+    		this.velocity+=0.05;
+    	}else{
+    		if(isKeyPressed[key_left]){
+    			this.velocity-=0.05;
+    		}else{
+    			this.velocity=0;
+    		}	
+  		}
+  		if(this.velocity>=this.maxVelocity){
+  			this.velocity=this.maxVelocity;
+  		}
+  		if(this.velocity==-this.maxVelocity){
+  			this.velocity=-this.maxVelocity;
+  		}
+  		this.x+=this.velocity;
+	}
+
+	draw(){
+		context.drawImage(poop, this.x, this.y, this.sx, this.sy);
+	}
+}
+
+class Platform{
+	constructor(x, y){
+		this.x=x;
+		this.y=y;
+	}
+}
+
+var MyPlayer=new Player(10, 10, 30, 30);
 
 function update() {
-    myX = myX+(mouseX-myX)/10;
-    myY = myY+(mouseY-myY)/10;
+    MyPlayer.update();
 }
 
 function draw() {
     // This is how you draw a rectangle
-    context.fillRect(myX, myY, 30, 30);
+    context.fillStyle="black";
+    context.fillRect(0, 0, 1366, 700);
+    MyPlayer.draw();
+
 };
 
 function keyup(key) {
